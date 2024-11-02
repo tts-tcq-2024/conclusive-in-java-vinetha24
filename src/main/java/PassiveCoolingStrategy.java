b33263480a9565e481b530c0a5b243b3efc818a0
vinetha24 <113199878+vinetha24@users.noreply.github.com>
2024-11-02 22:11:55 +0530

    Update and rename PassiveCoolingStrategy.java to Test.java

--- src/main/java/PassiveCoolingStrategy.java
+++
@@ -1,6 +0,0 @@
-public class PassiveCoolingStrategy implements TemperatureClassificationStrategy {
-    @Override
-    public BreachType classifyTemperatureBreach(double temperatureInC) {
-        return TypewiseAlert.inferBreach(temperatureInC, 0, 35);
-    }
-}

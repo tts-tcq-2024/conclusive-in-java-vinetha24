45d5270e7fa812b8e96b0c3f893e25abd1119e94
vinetha24 <113199878+vinetha24@users.noreply.github.com>
2024-11-02 22:10:51 +0530

    Update and rename MediumActiveCoolingStrategy.java to Test

--- src/main/java/MediumActiveCoolingStrategy.java
+++
@@ -1,7 +0,0 @@
-package TypewiseAlert
-public class MediumActiveCoolingStrategy implements TemperatureClassificationStrategy {
-    @Override
-    public BreachType classifyTemperatureBreach(double temperatureInC) {
-        return TypewiseAlert.inferBreach(temperatureInC, 0, 40);
-    }
-}

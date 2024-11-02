321f0be19156d51bae542a5312a5cd0616912744
vinetha24 <113199878+vinetha24@users.noreply.github.com>
2024-11-02 22:13:12 +0530

    Update MediumActiveCoolingStrategy.java

--- src/main/java/TypewiseAlert/MediumActiveCoolingStrategy.java
+++ src/main/java/TypewiseAlert/MediumActiveCoolingStrategy.java
@@ -1,4 +1,4 @@
-public class MediumActiveCoolingStrategy implements TemperatureClassificationStrategy {
+public class MediumActiveCoolingStrategy implements ITemperatureClassificationStrategy {
     @Override
     public BreachType classifyTemperatureBreach(double temperatureInC) {
         return TypewiseAlert.inferBreach(temperatureInC, 0, 40);
